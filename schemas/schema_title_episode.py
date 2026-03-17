8ccf9ed0096f77ebd2d050804cc1c1a344dc700b
SofiaPasichko <146076262+SofiaPasichko@users.noreply.github.com>
2026-03-17 22:16:51 +0200

    Revert "Add extraction stage for name.basics dataset"

--- schemas/schema_title_episode.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql.types import StructType, StructField, StringType
-
-title_episode_schema = StructType([
-    StructField("tconst", StringType(), True),
-    StructField("parentTconst", StringType(), True),
-    StructField("seasonNumber", StringType(), True),
-    StructField("episodeNumber", StringType(), True)
-])
